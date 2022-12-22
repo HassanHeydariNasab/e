a208efe55abe916487ea07e093df17adf040e1ab
HassanHeydariNasab <hsn6@tuta.io>
2022-12-22 21:08:46 +0330

    Init

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+import React from "react";
+
+const Home: NextPage = () => {
+  return (
+    <main>
+      <h1>Home</h1>
+    </main>
+  );
+};
+
+export default Home;
